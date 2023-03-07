741d5d94f6b006116fea2a657b590636290bacb6
Ivan Malkovskii <gotikym9@mail.ru>
2023-03-07 13:08:03 +0300

    third try

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,10 +6,33 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Slider _healthBarSlider;
 
+    private float _maxValue = 100;
+    private float _minValue = 0;
     private float _timeChange = 1f;
+    private float _changeValue = 10;
 
-    public void ChangeBar(float helth)
+    private float _currentValue;
+
+    private void Start()
     {
-        _healthBarSlider.DOValue(helth,_timeChange);
+        _currentValue = _maxValue;
+    }
+
+    public void Up()
+    {
+        if (_currentValue < _maxValue)
+        {
+            _currentValue += _changeValue;
+            _healthBarSlider.DOValue(_currentValue, _timeChange);
+        }
+    }
+
+    public void Down()
+    {
+        if (_currentValue > _minValue)
+        {
+            _currentValue -= _changeValue;
+            _healthBarSlider.DOValue(_currentValue, _timeChange);
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,30 +1,37 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private HealthBar _healthBar;
+    [SerializeField] private UnityEvent _healthUp;
+    [SerializeField] private UnityEvent _healthDown;
 
+    private float _heal = 10f;
+    private float _damage = 10f;
     private float _health = 100;
     private float _maxHealth = 100;
     private float _minHealth = 0;
 
-    public void TakeDamage(float valueChange)
+    public float ConveyMeaningHealth()
+    {
+        return _health;
+    }
+
+    private void TakeDamage()
     {
         if (_health > _minHealth)
         {
-            _health -= valueChange;
+            _health -= _damage;
+            _healthDown?.Invoke();
         }
-
-        _healthBar.ChangeBar(_health);
     }
 
-    public void TakeHeal(float valueChange)
+    private void TakeHeal()
     {
         if (_health < _maxHealth)
         {
-            _health += valueChange;
+            _health += _heal;
+            _healthUp?.Invoke();
         }
-
-        _healthBar.ChangeBar(_health);
     }
 }
